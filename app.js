27c5ffc378ee88b9a393482596fe36328a966362
acesuu <ahnafakib@iut-dhaka.edu>
2023-02-09 12:39:57 +0600

    modified the app.js file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express()
+require("dotenv").config({path:"./.env"})
 
 app.get('/', (req,res)=>{
     res.send("hello world");
 })
 
-app.listen(8000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("server is listening to port 8000......");
 })
